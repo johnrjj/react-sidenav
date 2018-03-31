e6fdaeb608f7b7302acefb4bec1cfd6337c5ab09
John Johnson <johnrjj@gmail.com>
2018-03-31 11:10:55 -0400

    Fill visual gap on transition (#1)
    * Visual gaps filled in

--- src/App.js
+++ src/App.js
@@ -1,44 +1,70 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { ArrowLeft, ArrowRight } from 'react-feather';
-import { SideNavPage, SideMenuShadowbox, SideMenu } from './components/SideNav';
+import { ArrowLeft, ArrowRight, Menu } from 'react-feather';
+import { SideNavPage, SideNavShadowbox, SideNav } from './components/SideNav';
 
 const AppContainer = styled.div``;
+const MainContainer = styled.div``;
 
 class App extends Component {
   constructor() {
     super();
+    this.generateNewSideNavPage = this.generateNewSideNavPage.bind(this);
     this.state = {
-      stack: [this.generateNewSideNavPage({ hideBack: true })],
+      sideNavOpen: true,
+      sideNavStack: [this.generateNewSideNavPage({ disableBack: true })],
     };
   }
 
   navigateForward = () =>
     this.setState(state => ({
-      stack: [...state.stack, this.generateNewSideNavPage()],
+      sideNavStack: [...state.sideNavStack, this.generateNewSideNavPage()],
     }));
 
   navigateBackward = () =>
-    this.setState(state => ({ stack: this.state.stack.slice(0, -1) }));
+    this.setState(state => ({
+      sideNavStack: this.state.sideNavStack.slice(0, -1),
+    }));
 
   render() {
+    const { sideNavOpen, sideNavStack } = this.state;
     return (
       <AppContainer>
-        <SideMenuShadowbox visible>
-          <SideMenu>{this.state.stack}</SideMenu>
-        </SideMenuShadowbox>
+        <SideNavShadowbox
+          onClick={() => this.closeSideNav()}
+          visible={sideNavOpen}
+        >
+          <SideNav open={sideNavOpen}>{sideNavStack}</SideNav>
+        </SideNavShadowbox>
+        <MainContainer>
+          <Menu onClick={() => this.openSideNav()} />
+        </MainContainer>
       </AppContainer>
     );
   }
 
+  openSideNav() {
+    this.setState({
+      sideNavOpen: true,
+    });
+  }
+
+  closeSideNav() {
+    this.setState({
+      sideNavOpen: false,
+    });
+  }
+
   // util fn
   sideNavCount = 0;
-  generateNewSideNavPage = ({ hideBack } = { hideBack: false }) => (
+  generateNewSideNavPage = ({ disableBack } = { disableBack: false }) => (
     <SideNavPage
       style={{ background: gradients[this.sideNavCount % 4] }}
       key={++this.sideNavCount}
     >
-      {!hideBack && <ArrowLeft onClick={() => this.navigateBackward()} />}
+      {!disableBack ? (
+        <ArrowLeft onClick={() => this.navigateBackward()} />
+      ) : null}
       <ArrowRight onClick={() => this.navigateForward()} />
     </SideNavPage>
   );
@@ -48,7 +74,7 @@ const gradients = [
   'linear-gradient(#ed6153, #ed6153 50%, #f9b16d)',
   'linear-gradient(#ad5389, #ad5389 50%, #3c1053)',
   'linear-gradient(#108dc7, #108dc7 50%, #ef32d9)',
-  'linear-gradient(#1c92d2, #ed6153 50%, #f2fcfe)',
+  'linear-gradient(#1c92d2, #ed6153 50%, #ed6153)',
 ];
 
 export default App;
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,7 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import posed, { PoseGroup } from 'react-pose';
 
-const SideMenuShadowbox = styled.aside`
+const BOUNCE_OFFSET_LEFT = '50px';
+const BOUNCE_OFFSET_RIGHT = '20px';
+
+const SideNavContainerPose = posed.div({
+  open: { x: '0%', staggerChildren: 100 },
+  closed: { x: '-100%' },
+});
+
+const SlideableMenuContainer = styled(SideNavContainerPose)`
+  position: absolute;
+  left: 0px;
+  top: 0;
+  box-sizing: content-box;
+  margin-left: -${BOUNCE_OFFSET_LEFT};
+  padding-left: ${BOUNCE_OFFSET_LEFT};
+  max-width: 340px;
+  width: 90%;
+  height: 100%;
+  display: flex;
+  contain: strict;
+  overflow: visible;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.5);
+  flex-direction: column;
+  will-change: transform;
+  z-index: 160;
+  pointer-events: auto;
+  /* width: 100%; */
+`;
+
+const SideNavShadowbox = styled.aside`
   color: #fff;
   position: fixed;
   left: 0;
@@ -29,17 +58,17 @@ const SideMenuShadowbox = styled.aside`
 `;
 
 const SideNavPagePose = posed.div({
-  enter: { x: '0%' },
-  exit: { x: '120%' },
+  enter: { x: '0%', staggerChildren: 1000 },
+  exit: { x: '110%' },
 });
 
 const SideNavPage = styled(SideNavPagePose)`
   position: absolute;
   top: 0;
-  left: 0;
+  left: 0px;
   display: flex;
   flex-direction: column;
-  max-width: 300px;
+  overflow: hidden;
   width: 100%;
   height: 100%;
   background: linear-gradient(#ed6153, #ed6153 50%, #f9b16d);
@@ -48,28 +77,23 @@ const SideNavPage = styled(SideNavPagePose)`
   will-change: transform;
   z-index: 160;
   pointer-events: auto;
+  box-sizing: content-box;
+  word-wrap: break-word;
+  padding-left: ${BOUNCE_OFFSET_LEFT};
+  padding-right: ${BOUNCE_OFFSET_RIGHT};
+  margin-right: -${BOUNCE_OFFSET_RIGHT};
   /* box-shadow: 0 2px 6px rgba(0, 0, 0, 0.5); */
   /* width: 90%; for box-shadow (due to display: hidden) */
 `;
 
-const SlideableMenuContainer = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  max-width: 300px;
-  width: 90%;
-  height: 100%;
-  display: flex;
-  contain: strict;
-  overflow: hidden;
-  z-index: 160;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.5);
-`;
-
-const SideMenu = ({ children }) => (
-  <SlideableMenuContainer>
+const SideNav = ({ open, children, onClick, ...rest }) => (
+  <SlideableMenuContainer
+    pose={open ? 'open' : 'closed'}
+    onClick={e => e.stopPropagation()}
+    {...rest}
+  >
     <PoseGroup>{children}</PoseGroup>
   </SlideableMenuContainer>
 );
 
-export { SideMenu, SideNavPage, SideMenuShadowbox };
+export { SideNav, SideNavPage, SideNavShadowbox };
